d248093dee89f71a66c5c62313725ef193f58e65
thegreatzhang <69706194+thegreatzhang@users.noreply.github.com>
2023-09-14 16:24:19 +0800

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,4 +5,8 @@ if status is-interactive
     set -xg https_proxy http://127.0.0.1:7890
     set -xg http_proxy http://127.0.0.1:7890
     set -xg all_proxy socks5://127.0.0.1:7891
+
+    function fish_greeting
+        echo Stand up Soldier! Time is (set_color yellow; date +%T; set_color normal)
+    end
 end

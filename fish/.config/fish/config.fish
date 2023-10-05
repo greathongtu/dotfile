9a6b8efee3f7b1ed476cd8747065aaafdf472eec
thegreatzhang <nicolasshuai@gmail.com>
2023-10-06 00:23:33 +0800

    modify fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -gx PATH /home/aaron/Downloads/clion-2023.2.1/bin $PATH
-    set -gx PATH $HOME/.cargo/bin $PATH 
-    set -gx PATH $HOME/Downloads/ $PATH
-    set -gx PATH $HOME/build/ $PATH
     set -gx PATH $HOME/go/bin $PATH
 
     set -xg https_proxy http://127.0.0.1:7890

0493ac10667e33c7e81efc63f95dfc3fca655f67
thegreatzhang <nicolasshuai@gmail.com>
2023-10-12 01:45:29 +0800

    fullscreen wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -18,7 +18,7 @@ config.font = wezterm.font 'FiraCodeNerdFont'
 
 wezterm.on('gui-startup', function(cmd)
   local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+  window:gui_window():toggle_fullscreen()
 end)
 
 return config

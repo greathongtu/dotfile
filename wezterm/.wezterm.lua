cf0862508f34fe7d6ad338a36d08bae20ff59522
thegreatzhang <nicolasshuai@gmail.com>
2023-10-19 15:13:21 +0800

    set wez font size = 19

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -15,6 +15,7 @@ end
 config.color_scheme = 'Catppuccin Frappe'
 -- config.font = wezterm.font 'JetBrains Mono'
 config.font = wezterm.font 'FiraCodeNerdFont'
+config.font_size = 19.0
 config.window_padding = {
   left = 0,
   right = 0,

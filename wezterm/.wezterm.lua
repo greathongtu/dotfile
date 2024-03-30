803bdb894461c8068ab2683daabe1c5b6486fabd
thegreatzhang <nicolasshuai@gmail.com>
2024-03-31 01:04:50 +0800

    change wezterm font to inconsolata

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local mux = wezterm.mux
 -- This table will hold the configuration.
@@ -8,30 +8,29 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Catppuccin Latte'
-config.font = wezterm.font 'FiraCodeNerdFont'
+config.color_scheme = "Catppuccin Latte"
+-- config.font = wezterm.font 'FiraCodeNerdFont'
+config.font = wezterm.font("InconsolataNerdFont")
 -- config.font = wezterm.font 'JetBrains Mono'
 -- this is for Windows
 -- config.font = wezterm.font 'FiraCode Nerd Font Mono'
 
-config.font_size = 17.0
+config.font_size = 18.0
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0;
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
-wezterm.on('gui-startup', function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():toggle_fullscreen()
--- For Windows
--- window:gui_window():maximize()
-    
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():toggle_fullscreen()
+	-- For Windows
+	-- window:gui_window():maximize()
 end)
 
 return config
-

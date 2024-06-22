674d0ec1cb1208e4d5d08bc214f184dc06eb0133
thegreatzhang <nicolasshuai@gmail.com>
2024-06-22 22:43:25 +0800

    space b find buffers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -187,15 +187,14 @@ require("lazy").setup({
 			-- See `:help telescope.builtin`
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
-			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
 			vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "[F]ind [F]iles" })
 
 			vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[F]ind [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
 			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
-			vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
 			vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+			vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "<leader>/", function()
@@ -571,19 +570,10 @@ require("lazy").setup({
 		opts = { signs = false },
 	},
 
-	-- {
-	-- 	-- Set lualine as statusline
-	-- 	"nvim-lualine/lualine.nvim",
-	-- 	-- See `:help lualine.txt`
-	-- 	opts = {
-	-- 		options = {
-	-- 			icons_enabled = false,
-	-- 			theme = "onedark",
-	-- 			component_separators = "|",
-	-- 			section_separators = "",
-	-- 		},
-	-- 	},
-	-- },
+	{
+		"f-person/git-blame.nvim",
+	},
+
 	{
 		"echasnovski/mini.nvim",
 		config = function()
